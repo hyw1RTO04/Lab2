5e0bd58dd91978eb336f428ca636597d7ddb1be0
user <user@WIN-N7P1J9L4V2S>
2025-09-30 15:41:53 +0300

    Добавил методы DoPrintCommand и GetDistance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Game
+{
+    private void DoPrintCommand()
+    {
+        //вывод позиций игроков и расстояния между ними (если это возможно)
+        string cat_location, mouse_location, distance;
+
+        if (cat.IsInGame())
+        {
+            cat_location = cat.GetLocation().ToString();
+        }
+        else
+        {
+            cat_location = "??";
+        }
+
+        if (mouse.IsInGame())
+        {
+            mouse_location = mouse.GetLocation().ToString();
+        }
+        else
+        {
+            mouse_location = "??";
+        }
+
+        if (cat.IsInGame() && mouse.IsInGame())
+        {
+            distance = GetDistance().ToString();
+        }
+        else
+        {
+            distance = "??";
+        }
+
+        Console.WriteLine($"{cat_location,-7}{mouse_location,-9}{distance}");
+    }
+
+    private int GetDistance()
+    {
+        //найти расстояние между игроками cat.location - mouse.location
+        return Math.Abs(cat.GetLocation() - mouse.GetLocation());
+    }
+}
