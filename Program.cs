0679bbe5491007607e5be799396456e85717bc56
user <user@WIN-N7P1J9L4V2S>
2025-10-01 14:22:02 +0300

    Обновил эти же методы

--- Program.cs
+++ Program.cs
@@ -2,6 +2,43 @@
 
 class Game
 {
+    private void DoMoveCommand(char command, int steps)
+    {
+        switch (command)
+        {
+            case 'M': mouse.Move(steps); break;
+            case 'C': cat.Move(steps); break;
+        }
+        CheckGameStatus();
+    }
+
+    private void CheckGameStatus()
+    {
+
+        if (cat.IsInGame() && (cat.location < 0 || cat.location >= size))
+        {
+            cat.state = State.Looser;
+            if (mouse.IsInGame())
+                mouse.state = State.Winner;
+            state = GameState.End;
+        }
+
+        if (mouse.IsInGame() && (mouse.location < 0 || mouse.location >= size))
+        {
+            mouse.state = State.Looser;
+            if (cat.IsInGame())
+                cat.state = State.Winner;
+            state = GameState.End;
+        }
+
+        // Проверяем, поймал ли кот мышь
+        if (cat.IsInGame() && mouse.IsInGame() && cat.location == mouse.location)
+        {
+            cat.state = State.Winner;
+            mouse.state = State.Looser;
+            state = GameState.End;
+        }
+    }
     private void DoPrintCommand()
     {
         //вывод позиций игроков и расстояния между ними (если это возможно)
@@ -41,5 +78,19 @@ class Game
     {
         //найти расстояние между игроками cat.location - mouse.location
         return Math.Abs(cat.GetLocation() - mouse.GetLocation());
+    }
+
+    private void DoStartCommand(char player, int position)
+    {
+        if (player == 'C')
+        {
+            cat.location = position;
+            cat.state = State.Playing;
+        }
+        else if (player == 'M')
+        {
+            mouse.location = position;
+            mouse.state = State.Playing;
+        }
     }
 }
